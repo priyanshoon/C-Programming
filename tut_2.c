982dd63ac6e64b994a335bfa2d054907ce0ed76e
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2022-06-18 12:15:39 +0530

    first commit

---
+++ tut_2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int main() {
+    int a = 25;
+    char star = '*';
+    float pi = 3.14;
+
+    // Variables are case sensetive
+    // 1st character is alphabet or '_'
+    // no comma/blank space
+    // no other symbols other than "_"
+
+    /*
+    ------------------------------------------------------------
+    Data Types                                     Size in bytes 
+
+    Char or Signed Char                            1
+    Unsigend Char                                  1
+    int and signed int                             2
+    unsigned int                                   2
+    short int and unsigned short int               2
+    signed short int                               2
+    long int and signed long int                   4
+    unsigned long int                              4
+    float                                          4
+    double                                         8
+    long double                                    10
+    ------------------------------------------------------------
+    */
+
+    // Reserve Keywords
+
+    /*
+    ---------------------------------------------
+    auto        double      int         struct
+    break       else        long        switch
+    case        enum        register    typedef
+    char        extern      return      union
+    continue    for         signed      void
+    do          if          static      while
+    default     goto        sizeof      volatile
+    const       float       short       unsigned
+    ---------------------------------------------
+    */
+
+    
+
+    return 0;
+}
