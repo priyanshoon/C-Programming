2743381c335c7e349c9013dc7d854ea73febf5c1
Priyanshu Gupta <guptapriyanshu973@gmail.com>
2023-01-15 00:01:40 +0530

    yoyoyoyoyo

--- pointers.c
+++ pointers.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main(){
-    printf("Pointers in C/C++");
-    int a;
+struct Person
+{
+    char name[64];
+    int age;
+};
+
+int main(int argc, char *argv[])
+{
+    struct Person people[100];
+
+    struct Person *p_Person = &people;
+
+    int i = 0;
+    for(i=0; i<100; i++ ){
+        p_Person->age=0;
+        p_Person += sizeof(struct Person);
+        
+    }
+
     return 0;
 
 }
--- struct-tut.c
+++ struct-tut.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
+/*
 struct myStructure {
     int myNum;
     char myLetter;
 };
+*/
+
+struct employee
+{
+    int id;
+    char name[24];
+};
 
 int main() {
     // Create a structure variable of myStructure called s1
-    struct myStructure s1;
+    //    struct myStructure s1;
 
-    s1.myNum = 18;
-    s1.myLetter = 'B';
+    //s1.myNum = 18;
+    //  s1.myLetter = 'B';
+    
+    struct employee e1, e2;
+    e1.id = 1;
+    e2.id = 2;
+    strcpy(e1.name, "Priyanshu");
+    strcpy(e2.name, "Rounak");
+
+    printf("First employee name is %s and his id is %d \n", e1.name, e1.id);
+    printf("Second employee name is %s and his id is %d \n", e2.name, e2.id);
+
 
     return 0;
 }
